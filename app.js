61a4fbf069b852417b3427db9513b02eec2973fd
Nada <50797024+Nada-TB@users.noreply.github.com>
2020-04-15 22:25:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function(){
                             addClass(document.querySelectorAll('.selected'),'disabled');
                             removeClass(document.querySelectorAll('img'),'selected');
                             selectedCards=[];
-                        },500);
+                        },5000);
 
                         if(document.querySelectorAll('.disabled').length===12){
                             fetchResult(document.getElementById('comment'), 'congratulations, you win!');
